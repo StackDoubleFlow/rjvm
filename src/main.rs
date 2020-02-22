6e47f8fa22873b1228034b5df22922aae3582132
StackDoubleFlow <ojaslandge@gmail.com>
2020-02-21 21:46:50 -0600

    Human panic

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,16 @@ mod thread;
 #[macro_use]
 extern crate bitflags;
 
+#[macro_use]
+extern crate human_panic;
+
 // https://docs.oracle.com/javase/specs/jvms/se13/html/
 
 fn main() {
+    setup_panic!();
+}
+
+#[test]
+fn test() {
     println!("Hello, world!");
 }
--- src/thread.rs
+++ src/thread.rs
@@ -1,7 +1,7 @@
 
 
 struct Thread {
-
+    
 }
 
 struct OperandStack {
