70264dd098d46931bf79069cc1d8294e22fe9740
StackDoubleFlow <ojaslandge@gmail.com>
2020-02-22 21:17:44 -0600

    Use clap for cli

--- src/main.rs
+++ src/main.rs
@@ -8,10 +8,17 @@ extern crate bitflags;
 #[macro_use]
 extern crate human_panic;
 
+use clap::App;
+
 // https://docs.oracle.com/javase/specs/jvms/se13/html/
 
 fn main() {
     setup_panic!();
+    App::new("rjvm")
+        .version("0.1.0")
+        .about("A Java Virtual Machine completely written in rust.")
+        .author("StackDoubleFlow <ojaslandge@gmail.com>")
+        .get_matches();
 }
 
 #[test]
