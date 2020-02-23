02f6a28790958bf670e21f8307adb8c6926c6671
StackDoubleFlow <ojaslandge@gmail.com>
2020-02-22 22:06:13 -0600

    Use structopt instead of clap

--- src/main.rs
+++ src/main.rs
@@ -8,17 +8,23 @@ extern crate bitflags;
 #[macro_use]
 extern crate human_panic;
 
-use clap::App;
+use std::path::PathBuf;
+use structopt::StructOpt;
 
-// https://docs.oracle.com/javase/specs/jvms/se13/html/
+#[derive(StructOpt, Debug)]
+#[structopt(name = "rjvm", version = "0.1.0", author = "StackDoubleFlow <ojaslandge@gmail.com>", about = "A Java Virtual Machine completely written in rust.")]
+struct Opts {
+
+    /// Path to the main class file
+    #[structopt(parse(from_os_str))]
+    class_file: PathBuf
+}
 
 fn main() {
     setup_panic!();
-    App::new("rjvm")
-        .version("0.1.0")
-        .about("A Java Virtual Machine completely written in rust.")
-        .author("StackDoubleFlow <ojaslandge@gmail.com>")
-        .get_matches();
+    let opts = Opts::from_args();
+    let data = std::fs::read(opts.class_file).unwrap();
+    println!("{:#?}", class::Class::deserialize(data));
 }
 
 #[test]
