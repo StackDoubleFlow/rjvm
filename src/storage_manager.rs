6d2fe9b14c06af83937201342711798beac0a95f
StackDoubleFlow <ojaslandge@gmail.com>
2020-02-26 23:26:17 -0600

    WIP Storage

--- src/storage_manager.rs
+++ src/storage_manager.rs
@@ -1,4 +1,26 @@
+use crate::class::Class;
+use std::collections::HashMap;
+
 // How the openjdk does it:
 // http://openjdk.java.net/groups/hotspot/docs/StorageManagement.html
 
-struct MethodArea {}
+struct Storage {
+    method_area: MethodArea,
+    heap: Heap,
+}
+
+struct MethodArea {
+    classes: HashMap<String, Class>,
+}
+
+impl MethodArea {
+    fn new() -> MethodArea {
+        MethodArea {
+            classes: HashMap::new(),
+        }
+    }
+}
+
+struct Heap {}
+
+impl Heap {}
